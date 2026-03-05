5e35a630ebe10f09aff35d44823bce2da9efd546
Omit-m <omidmolla420@gmail.com>
2026-03-06 02:08:31 +0600

    code

---
+++ First part/Arithmetic_Operation.py
@@ -0,0 +1,21 @@
+a = 10 
+b = 5
+
+c = a + b
+print ( c )
+
+d = a - b 
+print( d )
+
+e = a * b 
+print( e )
+
+f = a // b 
+print ( f )
+
+"""
+for addition ( + )
+for divide ( // )
+for subtraction ( - )
+for multiplication ( * )
+"""
---
+++ First part/data_type.py
@@ -0,0 +1,13 @@
+I=10
+print(type(I))
+
+f=10.0
+print(type(f))
+
+s = " I am from Bangladesh"
+s1 = ''
+# whatever in single or double quotation is a string 
+print(type(s),print(s1))
+
+
+
