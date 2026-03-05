7e2f3bdf4fe7b08331f1c6f15c89acc12f5cec04
Omit-m <omidmolla420@gmail.com>
2026-03-05 13:41:16 +0600

    delet

--- text.py
+++
@@ -1,5 +0,0 @@
-sum=0
-for i in range(1,100,2):
- sum=sum+i
-print("sumation is=",sum)
-
--- tringlr.py
+++
@@ -1,4 +0,0 @@
-base=float(input("enter the valuev of base:"))
-hight=float(input("enter the valuev of hight:"))
-tringle_area=0.5*base*hight
-print('the tringle_area is=',tringle_area)
