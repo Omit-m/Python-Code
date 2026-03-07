fb8d309f7ff9f9227086ec8572c8cf0ac7064769
Omit-m <omidmolla420@gmail.com>
2026-03-07 12:36:16 +0600

    code

---
+++ First part/indexing.py
@@ -0,0 +1,15 @@
+numbers= [0,10,20,30,40,50,60,70,80,90]
+# print(numbers)
+# always INDEXING start form [0]
+
+print(numbers[3])
+print(numbers[9])
+
+
+"""
+print(numbers[10])
+output: 
+     print(numbers[10])
+     IndexError: list index out of range
+#  as the index end to 9 
+"""
