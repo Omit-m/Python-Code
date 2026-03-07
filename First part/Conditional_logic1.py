3b3da25f51ae8774cd040f010082801e7022c65d
Omit-m <omidmolla420@gmail.com>
2026-03-07 12:27:43 +0600

    code

--- First part/Conditional_logic1.py
+++ First part/Conditional_logic1.py
@@ -1,3 +1,19 @@
-number= 20
-a= number > 10 
-print ( a )
+number= 200
+# a= number > 10 
+# print ( a )
+
+# b= number < 10 
+# print( b )
+
+c= number > 10  and number < 10 
+print(c)
+
+d= number > 10  and 49 < number
+print( d )
+
+"""
+for (and) condition, when both condition are ture, only on this moment is ture, while the result is false. 
+
+for (or) condition, when both condition are false, only on this moment is false, while the result is true. 
+
+"""
