b5ffb4cb462bbd47088bb2a1944e77f659f1ba35
Omit-m <omidmolla420@gmail.com>
2026-03-07 13:18:08 +0600

    code

--- First part/If_statement.py
+++ First part/If_statement.py
@@ -1,7 +1,8 @@
-saarc = ["Bangladesh", "India", "Bhutan", "Nepal", "Afghanistan", "Pakistan", "Sri Lanka"]
+saarc = ["bangladesh", "india", "bhutan", "nepal", "afghanistan", "pakistan", "sri lanka"]
 
-country= input("Enter a country name : ")
+
+country= input("Enter a country name : ").lower()
 if country in saarc:
     print(country, "is a member of Saarc")
 
-print(country, " is not a member of Saarc")
+print("Program Terminated")
