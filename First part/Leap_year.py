a14d5f5df6abbfd3d606eb953496279369ee5bdf
Omit-m <omidmolla420@gmail.com>
2026-03-09 00:55:59 +0600

    File change

--- First part/Leap_year.py
+++ First part/Leap_year.py
@@ -1,11 +1,7 @@
-year= int(input("Enter a year: "))
+year = int( input( "Enter a year : "))
 
+if ( year % 400 ==0 ) or ( year % 100 !=0 and year % 4 ==0):
+    print(year, "is a leap year")
 
-if year % 400 ==0:
- print(year , " is a leap year")
-elif year % 100 ==0: 
-   print((year , " is not a leap year"))
-elif year % 4 ==0:
-   print((year , " is a leap year"))   
-else:
-   print(year , " is not a leap year")
+else :
+    print(year, "is not a leap year")    
