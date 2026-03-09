379a17b9af9b20eb23efdb9cc1e411021a51745f
Omit-m <omidmolla420@gmail.com>
2026-03-09 11:12:26 +0600

    random number

--- First part/Python For loop/Loop_3.py
+++ First part/Python For loop/Loop_3.py
@@ -2,14 +2,12 @@
 import random
 
 ls=[]
+maX_num=0
 for i in range (5):
     num= random.randint(10, 50)
     ls.append(num)
-   
 print(ls)
 
-# ls= [50, 19, 13, 15, 29]
-maX_num=0
 for n in ls:
     if n > maX_num:
         maX_num=n
