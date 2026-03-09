6483bfdf8b4907b9a5697829647b59e0978af63e
Omit-m <omidmolla420@gmail.com>
2026-03-09 11:06:35 +0600

    random number

--- First part/python_random_number.py
+++ First part/python_random_number.py
@@ -1,7 +1,18 @@
 import random
-ls=[]
-for i in range (51):
-    num = random.randint(1,50)
-    ls.append(num)
 
-print(ls)
+number= random.random()
+print(number)
+
+
+
+
+
+
+
+
+# ls=[]
+# for i in range (51):
+#     num = random.randint(1,50)
+#     ls.append(num)
+
+# print(ls)
