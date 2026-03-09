36e917813688a865fa609e0e49f07a1007f738e1
Omit-m <omidmilla420#gmail.com>
2026-03-09 12:26:25 +0600

    code

---
+++ First part/Introduce turtle/squer by turtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.exitonclick()
+
+for i in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+
+turtle.exitonclick()
