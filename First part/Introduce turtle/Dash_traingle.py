33bb3eceba46d6ad63ba4a4da2f9922ba818e2c0
Omit-m <omidmilla420#gmail.com>
2026-03-10 12:07:56 +0600

    code

--- First part/Introduce turtle/Dash_traingle.py
+++ First part/Introduce turtle/Dash_traingle.py
@@ -3,7 +3,6 @@ import turtle
 turtle.speed(2)
 turtle.shape('turtle')
 
-
 for i in range(3):
     for i in range(20):
         turtle.forward(10)
@@ -11,6 +10,5 @@ for i in range(3):
         turtle.forward(3)
         turtle.pendown()
     turtle.left(120)        
-
 
 turtle.exitonclick()
