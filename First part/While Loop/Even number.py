83b0c7ce79451ac5149f28834327ff63faae2bec
Omit-m <omidmolla420@gmail.com>
2026-03-12 13:51:53 +0600

    code

---
+++ First part/While Loop/Even number.py
@@ -0,0 +1,5 @@
+n=0
+m= int(input("Enter the last number : "))
+while n<=m:
+    print(n)
+    n+=2
---
+++ First part/While Loop/odd number.py
@@ -0,0 +1,7 @@
+
+n=1
+
+m= int(input("Enter the last number : "))
+while n<=m:
+    print(n)
+    n+=2
