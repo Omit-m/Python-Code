c3e6dfd55bb92a3c584a12e3f9d63c7a3af6568a
Omit-m <omidmolla420@gmail.com>
2026-03-14 23:24:10 +0600

    code

--- First part/Python Break and continue function/continue.py
+++ First part/Python Break and continue function/continue.py
@@ -1,5 +1,5 @@
-# for i in range(1,50):
-#     if i in range(35,41):
-#         continue
-#     #     continue is used to skip the current iteration and move to the next loop cycle.
-#     print(i)
+for i in range(1,50):
+    if i in range(35,41):
+        continue
+    #     continue is used to skip the current iteration and move to the next loop cycle.
+    print(i)
