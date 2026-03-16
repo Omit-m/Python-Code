910b2492ec638dc800c75100b5577bd06c607190
Omit-m <omidmolla420@gmail.com>
2026-03-16 21:40:58 +0600

    code

--- First part/Function/User define/Add_numbers.py
+++ First part/Function/User define/Add_numbers.py
@@ -1,27 +1,12 @@
-# n=int(input(" How many numbers you want to add ? "))
+n=int(input(" How many numbers you want to add ? "))
 
-# sum=0
-# def total ():
-#     global sum
-#     for i in range(n):
-#     number = int(input("Enter a number : "))
-#     sum += number
-
-# print(sum)
-
-
-n = int(input("How many numbers you want to add? "))
-
-total = 0
-
-def add_numbers():
-    global total
+sum=0
+def total ():
+    global sum
     for i in range(n):
-        number = int(input("Enter a number: "))
-        total += number
-    print(total)
+        number = int(input("Enter a number : "))
+        sum += number
+    print(sum)
 
-    
-add_numbers()
-
+total()
 
