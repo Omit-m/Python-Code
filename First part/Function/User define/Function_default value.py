77c461a2782f9bb33b0e19e33a3931ba04af0f27
Omit-m <omidmolla420@gmail.com>
2026-03-17 22:45:36 +0600

    code

--- First part/Function/User define/Function_default value.py
+++ First part/Function/User define/Function_default value.py
@@ -1,6 +1,13 @@
-def myfuc(x,y=10):
-    print("Y =" ,y)
+def myfuc(x,y=10,z=0):
+    print("x =" ,x,"Y =" ,y,"z =" ,z)
 
 x=20
-myfuc(x)
-myfuc()  
+y=6
+z=7
+
+myfuc(x,y,z) 
+print("omit")
+myfuc(x,y)
+print("omit1")
+myfuc(z)
+ 
