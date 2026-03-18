6eb006135df4568f1298d7a93f62dc1db27889ff
Omit-m <omidmolla420@gmail.com>
2026-03-18 13:20:12 +0600

    code

--- Basic/lll.py
+++
@@ -1,8 +0,0 @@
-user_input = input("Enter a number (press Enter for default): ")
-
-if user_input == "":
-    number = 10   # default value
-else:
-    number = int(user_input)
-
-print("Value is:", number)
