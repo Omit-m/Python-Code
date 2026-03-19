0f351d389409d913d206324e089ed38a9c24766c
Omit-m <omidmolla420@gmail.com>
2026-03-20 00:02:57 +0600

    comment

--- First part/Function/Build in /Capitalize.py
+++ First part/Function/Build in /Capitalize.py
@@ -3,6 +3,7 @@ Name = input("Enter you name : ")
 cp= Name.capitalize()
 print(cp)
 
+
 # 🧠 Key Points:
         # Only the first letter becomes capital
         # All other letters become small (lowercase)
