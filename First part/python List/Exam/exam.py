41ec6f2d072a937cbabee5aeb34123b1163cd264
Omit-m <omidmolla420@gmail.com>
2026-03-24 22:34:20 +0600

    code

---
+++ First part/python List/Exam/exam.py
@@ -0,0 +1,10 @@
+
+def palindrome_fun(word):
+
+    if word == word[::-1]:
+        print("It is a palindrome")
+    else:
+        print("It is not a palindrome")
+
+
+palindrome_fun(input("input a word : "))
--- First part/python List/indexing2.py
+++ First part/python List/indexing2.py
@@ -7,14 +7,5 @@
 
 
 
-def palindrome_fun(word):
-
-    if word == word[::-1]:
-        print("It is a palindrome")
-    else:
-        print("It is not a palindrome")
-
-
-palindrome_fun(input("input a word : "))
 
 
