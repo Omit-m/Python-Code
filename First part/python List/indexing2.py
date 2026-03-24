1513b09e35e20c84e46e79f1df9c95fe05e53c1f
Omit-m <omidmolla420@gmail.com>
2026-03-24 22:32:42 +0600

    code

--- First part/python List/indexing2.py
+++ First part/python List/indexing2.py
@@ -1,9 +1,20 @@
-my_list = [10, 20, 30, 40, 50]
+# my_list = [10, 20, 30, 40, 50]
 
 
-print(my_list[-1]) # 50 (last element)
-print(my_list[-2]) # 40 (Second last element)
+# print(my_list[-1]) # 50 (last element)
+# print(my_list[-2]) # 40 (Second last element)
 
 
+
+
+def palindrome_fun(word):
+
+    if word == word[::-1]:
+        print("It is a palindrome")
+    else:
+        print("It is not a palindrome")
+
+
+palindrome_fun(input("input a word : "))
 
 
