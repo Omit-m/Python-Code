81f99ee9441a385110c1ec963aa7cee740bf6674
Omit-m <omidmolla420@gmail.com>
2026-03-26 23:57:26 +0600

    code

--- First part/python List/List Comprehensions/comprehension1.py
+++ First part/python List/List Comprehensions/comprehension1.py
@@ -1,6 +1,5 @@
 # List comprehensions provide a concise way to create lists. It consists of brackets containing an expression followed by a for clause, then zero or more for or if clauses. The expressions can be anything, meaning you can put in all kinds of objects in lists. The result will be a new list resulting from evaluating the expression in the context of the for and if clauses which follow it. The list comprehension always returns a result list.
 
-
 # without list comprehension
 # li = [1,2,3,4,5,6,7,8,9,10]
 # new_li = []
