c7497eeab4954decab1f763ea39be1e11a43126e
Omit-m <omidmolla420@gmail.com>
2026-03-28 14:19:23 +0600

    code

--- First part/Python set/Union operation.py
+++ First part/Python set/Union operation.py
@@ -1,5 +1,6 @@
 set1= {1, 2, 3, 4, 5}
-set2= {1,2,3,4,5,6,7,8,9,10}
+set2= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+set3= {11,12,13,14,15}
 
-union = set1.union(set2)
+union = set1.union(set2, set3  )    
 print(union)
