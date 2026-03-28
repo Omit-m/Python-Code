c4bb3929e606478ec1e5159980d2042d2355d3da
Omit-m <omidmolla420@gmail.com>
2026-03-28 14:10:19 +0600

    delete

--- First part/Python set/Type conversion.py
+++ First part/Python set/Type conversion.py
@@ -4,6 +4,6 @@ a = tuple(data)   # list → tuple
 b = set(data)     # list → set
 c = list(b)       # set → list
 
-print(a)   # (1, 2, 2, 3)
-print(b)   # {1, 2, 3}
-print(c)   # [1, 2, 3]
+print(a)   
+print(b)  
+print(c)  
